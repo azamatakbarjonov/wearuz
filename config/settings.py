a6f1d100136793590b5c9f79bf817d13afa8568b
azamatakbarjonov <akbarjonovazamat1@gmail.com>
2025-08-23 22:01:49 +0500

    Add widget-tweaks to requirements

--- config/settings.py
+++ config/settings.py
@@ -38,17 +38,25 @@ INSTALLED_APPS = [
 
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # eng yuqoriga qo‘yiladi
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://wearuz.onrender.com",
+]
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
